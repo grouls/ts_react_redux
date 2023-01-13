0995fc870d2718096e6d8a203f672cb6c2add7fd
GARY ROUSLTONE <gary.roulstone@repknight.com>
2023-01-13 14:32:50 +0000

    create actions & structure store

--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,5 +1,10 @@
+import { Todo }            from '../actions'
 import { combineReducers } from "redux";
+import { todosReducer }    from "./todos";
+export interface StoreState {
+    todos: Todo[];
+}
 
-export const reducers = combineReducers({
-    counter: () => 1
+export const reducers = combineReducers<StoreState>({
+    todos: todosReducer
 });
