3db1a547f9232419ac7c65d35b304b4d7ee32535
GARY ROUSLTONE <gary.roulstone@repknight.com>
2023-01-16 10:56:20 +0000

    hook up redux with connect

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { Todo, fetchTodos } from '../actions';
 
-export class App extends React.Component {
+import React                from 'react';
+import { StoreState }       from '../reducers';
+import { connect }          from 'react-redux';
+
+interface AppProps {
+	todos: Todo[];
+	fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+	componentDidMount(): void {
+		this.props.fetchTodos();
+	}
 	render() {
 		return <div>Hello World</div>;
 	}
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+	return { todos: todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
